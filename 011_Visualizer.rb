1f8fde3935903f256a2d8d11a6ff91038c0940fd
Justin Piotroski <justin.piotroski@gmail.com>
2016-08-19 00:12:01 -0700

    More P11 work, added a visualizer to help debug and present the data

--- 011_Visualizer.rb
+++ 011_Visualizer.rb
@@ -1,9 +1,32 @@
-#!/usr/bin/env ruby
+class String
+	def black;          "\e[30m#{self}\e[0m" end
+	def red;            "\e[31m#{self}\e[0m" end
+	def green;          "\e[32m#{self}\e[0m" end
+	def brown;          "\e[33m#{self}\e[0m" end
+	def blue;           "\e[34m#{self}\e[0m" end
+	def magenta;        "\e[35m#{self}\e[0m" end
+	def cyan;           "\e[36m#{self}\e[0m" end
+	def gray;           "\e[37m#{self}\e[0m" end
+
+	def bg_black;       "\e[40m#{self}\e[0m" end
+	def bg_red;         "\e[41m#{self}\e[0m" end
+	def bg_green;       "\e[42m#{self}\e[0m" end
+	def bg_brown;       "\e[43m#{self}\e[0m" end
+	def bg_blue;        "\e[44m#{self}\e[0m" end
+	def bg_magenta;     "\e[45m#{self}\e[0m" end
+	def bg_cyan;        "\e[46m#{self}\e[0m" end
+	def bg_gray;        "\e[47m#{self}\e[0m" end
+
+	def bold;           "\e[1m#{self}\e[22m" end
+	def italic;         "\e[3m#{self}\e[23m" end
+	def underline;      "\e[4m#{self}\e[24m" end
+	def blink;          "\e[5m#{self}\e[25m" end
+	def reverse_color;  "\e[7m#{self}\e[27m" end
+end
 
 full_number = "08022297381500400075040507785212507791084949994017811857608717409843694804566200814931735579142993714067538830034913366552709523046011426924685601325671370236912231167151676389419236542240402866331380244732609903450244753353783684203517125032988128642367102638406759547066183864706726206802621220956394396308409166499421245558056673992697177878968314883489637221362309750076442045351400613397343133957817532822753167159403800462161409535692163905429635314755588824001754243629855786560048357189070544443744602158515417581980816805944769287392138652177704895540045208839735991607975732162626793327986688366887576220720346336746551232639353690442167338253911249472180846293240627636206936417230238834629969826759857404361620733529783190017431497148868116235705540170547183515469169233486143520189196748"
 
-
-
+colortable = []
 
 def construct_grid(width, height, number)
 	# row, column
@@ -19,7 +42,7 @@ def construct_grid(width, height, number)
 	while start_point < str_length
 		char_string = number[start_point,2]
 		subarray << char_string.to_i
-
+		# puts char_string
 		if subarray.length == 20
 			grid << subarray
 			subarray = []
@@ -31,16 +54,41 @@ def construct_grid(width, height, number)
 	return grid
 
 end
-def pretty_print(grid)
-	grid.each do |subarray|
-		subarray.each do |value|
+
+def pretty_print(grid, colors)
+	grid.each_with_index do |subarray, i|
+		subarray.each_with_index do |value, j|
+
+			value_string = ""
+
 			if value.to_s.length == 1
-				print "0#{value} "
+				value_string = "0#{value} "
 			else
-				print "#{value} "
+				value_string = "#{value} "
+			end
+
+			if colors[i][j] == true
+				print value_string.green
+			else
+				print value_string
 			end
 		end
 		puts
+	end
+end
+
+def parse_complete(number, width, height)
+	reference = 0
+	start_index = 0
+	end_index = 1
+
+	while end_index < number.length
+		# Things happen
+
+		stash = 
+
+		start_index += 1
+		end_index += 1
 	end
 end
 
@@ -139,15 +187,17 @@ def get_r_diagonals(grid)
 	return parse_array staging_array
 end
 
-#METHODS ABOVE, PROCEDURE BELOW#
 the_grid = construct_grid 20, 20, full_number
-values_array = []
+colortable = Array.new(the_grid)
 
-values_array.push(get_horizontals the_grid)
-values_array.push(get_verticals the_grid)
-values_array.push(get_r_diagonals the_grid)
-values_array.push(get_l_diagonals the_grid)
+colortable.each_with_index do |subarray, i|
+	subarray.each_with_index do |value, j|
+		if(the_grid[i][j] % 5 == 0)
+			colortable[i][j] = false
+		end
+	end
+end
 
-pretty_print the_grid
 puts
+pretty_print the_grid, colortable
 
